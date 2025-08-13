ca9b13c2650d00c8d41716abe2e1dcdfd398b0a3
Blue-Leaf-vm <yellowaholotle@gmail.com>
2025-08-14 01:56:13 +0900

    feat : header html 구조 완성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as Notification } from './notification.svg';
+import { ReactComponent as Search } from './search.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme="light">
+      <div className='Content'>
+        <div className='HomeLayout'>
+          <header className="Header">
+            <div className='Header_inner'>
+              <div className='Header_logo'>
+                <a herf="/">
+                  <Logo></Logo>
+                </a>
+              </div>
+              <div className='Header_right'>
+                <a className='Header_notification' herf="/notification">
+                  <div className='Header_icon'>
+                    <Notification></Notification>
+                  </div>
+                </a>
+                <a className='Header_search' herf="/search">
+                  <div className='Header_icon'>
+                    <Search></Search>
+                  </div>
+                </a>
+                <button className='RoundButton RoundButton_default RoundButton_notBorder RoundButton_darkGray Header_button'>로그인</button>
+              </div>
+            </div>
+          </header>
+        </div>
+      </div>
     </div>
   );
 }
