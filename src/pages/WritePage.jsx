f5b2ac2ff873758671814230152e1933722dc2f8
Blue-Leaf-vm <yellowaholotle@gmail.com>
2025-08-21 01:41:34 +0900

    refactor : 스타일 모듈화

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -6,52 +6,50 @@ import CodeToolbar from '../components/codeToolbar';
 
 import CodeMirror from '@uiw/react-codemirror';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
-//import styles from './WritePage.module.css';
-import './WritePage.css'
+import styles from './WritePage.module.css';
 
 function WritePage() {
-  const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState([]);
 
   return (
-    <div className='Writemain'>
-      <div className='WriteLayout'>
-        <div className='WriteLeft'>
-          <div className='CodeBox'>
-            <div className='CodeWrapper'>
-              <div className='CodeTop'>
-                <div className='Topbox'>
-                  <textarea className='TitleInput' placeholder='제목을 입력하세요'></textarea>
-                  <div className='Line'></div>
+    <div className={styles.Writemain}>
+      <div className={styles.WriteLayout}>
+        <div className={styles.WriteLeft}>
+          <div className={styles.CodeBox}>
+            <div className={styles.CodeWrapper}>
+              <div className={styles.CodeTop}>
+                <div className={styles.Topbox}>
+                  <textarea className={styles.TitleInput} placeholder='제목을 입력하세요'></textarea>
+                  <div className={styles.Line}></div>
                   <div>
-                    <div className='TagBox'>
-                      <div className='Tag'>Placeholder</div>
-                      <input className="TagInput" placeholder='태그를 입력하세요'></input>
+                    <div className={styles.TagBox}>
+                      <div className={styles.Tag}>Placeholder</div>
+                      <input className={styles.TagInput} placeholder='태그를 입력하세요'></input>
                     </div>
                   </div>
                 </div>
               </div>
               <CodeToolbar></CodeToolbar>
-              <CodeMirror className="CodeMirror" placeholder="당신의 이야기를 적어보세요..." extensions={[markdown({ base: markdownLanguage })]} basicSetup={{lineNumbers: false, highlightActiveLineGutter: false, highlightActiveLine: false}} />
+              <CodeMirror className={styles.CodeMirror} placeholder="당신의 이야기를 적어보세요..." extensions={[markdown({ base: markdownLanguage })]} basicSetup={{lineNumbers: false, highlightActiveLineGutter: false, highlightActiveLine: false}} />
             </div>
-            <div className='Toolbar'>
-              <div className='ToolBox'>
-                <button className='ExitButton'>
+            <div className={styles.Toolbar}>
+              <div className={styles.ToolBox}>
+                <button className={styles.ExitButton}>
                   <Leftarrow></Leftarrow>
                   <span>나가기</span>
                 </button>
-                <div className='SaveButtons'>
-                  <button className='TempSave Button SaveButton'>임시저장</button>
-                  <button className='Save Button SaveButton'>출간하기</button>
+                <div className={styles.SaveButtons}>
+                  <button className={`${styles.TempSave} ${styles.Button} ${styles.SaveButton}`}>임시저장</button>
+                  <button className={`${styles.Save} ${styles.Button} ${styles.SaveButton}`}>출간하기</button>
                 </div>
               </div>
             </div>
           </div>
         </div> 
-        <div className='WriteRight'>
-          <div className='WritePreview'>
-            <h1 className='WriteTitle'></h1>
-            <div className='WriteContents'>
-
+        <div className={styles.WriteRight}>
+          <div className={styles.WritePreview}>
+            <h1 className={styles.WriteTitle}></h1>
+            <div className={styles.WriteContents}>
             </div>
           </div>
         </div>
