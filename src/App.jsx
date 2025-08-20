5161da5a82ea1fd56ba4269d47b1b93053fa4bb5
Blue-Leaf-vm <yellowaholotle@gmail.com>
2025-08-20 23:57:33 +0900

    feat : 쓰기 페이지 뼈대 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PostPage from './pages/PostPage';
+import WritePage from './pages/WritePage';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/:userid/:postid" element={<PostPage/>} />
+        <Route path="/write" element={<WritePage/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/WritePage.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ReactMarkdown from "react-markdown";
+import { ReactComponent as Leftarrow } from '../assets/leftarrow.svg';
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+//import styles from './WritePage.module.css';
+import './WritePage.css'
+
+function WritePage() {
+  const [posts, setPosts] = useState([]);
+
+  return (
+    <div className='Writemain'>
+      <div className='WriteLayout'>
+        <div className='WriteLeft'>
+          <div className='CodeBox'>
+            <div className='CodeWrapper'>
+              <div className='CodeTop'>
+
+              </div>
+              <div className='CodeToolbar'>
+
+              </div>
+              <CodeMirror className="CodeMirror" placeholder="당신의 이야기를 적어보세요..." extensions={[markdown({ base: markdownLanguage })]} basicSetup={{lineNumbers: false, highlightActiveLineGutter: false, highlightActiveLine: false}} />
+            </div>
+            <div className='Toolbar'>
+              <div className='ToolBox'>
+                <button className='ExitButton'>
+                  <Leftarrow></Leftarrow>
+                  <span>나가기</span>
+                </button>
+                <div className='SaveButtons'>
+                  <button className='TempSave Button SaveButton'>임시저장</button>
+                  <button className='Save Button SaveButton'>출간하기</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        <div className='WriteRight'>
+          <div className='WritePreview'>
+            <h1 className='WriteTitle'></h1>
+            <div className='WriteContents'>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WritePage;
